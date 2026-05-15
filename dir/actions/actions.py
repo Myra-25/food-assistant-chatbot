0743b2f514e2c72cb9377e5d87206338204a4564
Sakshi <sakshipg2508@gmail.com>
2026-05-15 10:30:54 +0530

    Added custom actions and login hint

--- dir/actions/actions.py
+++ dir/actions/actions.py
@@ -6,6 +6,7 @@
 
 from rasa_sdk import Action
 from rasa_sdk.executor import CollectingDispatcher
+import requests
 
 
 class ActionTrackOrder(Action):
@@ -15,9 +16,36 @@ class ActionTrackOrder(Action):
 
     def run(self, dispatcher, tracker, domain):
 
-        dispatcher.utter_message(
-            text="📦 Your latest order is currently being prepared."
-        )
+        try:
+
+            # FETCH ORDERS FROM FASTAPI
+            response = requests.get(
+                "http://127.0.0.1:8000/orders"
+            )
+
+            orders = response.json()
+
+            if len(orders) == 0:
+
+                dispatcher.utter_message(
+                    text="📦 No orders found."
+                )
+
+                return []
+
+            latest = orders[-1]
+
+            dispatcher.utter_message(
+
+                text=f"📦 Your {latest['item']} order from {latest['restaurant']} is currently {latest['status']}."
+
+            )
+
+        except Exception as e:
+
+            dispatcher.utter_message(
+                text="⚠️ Unable to track order right now."
+            )
 
         return []
 # This is a simple example for a custom action which utters "Hello World!"
